b4a7f05f960a287bf56d35b8016dd574283dcf06
Ali <work.gh.2021@gmail.com>
2024-12-31 13:16:34 +0330

    login popup part 1

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-// animation border-bottom (for header items)
+const menuItems = document.querySelectorAll('.nav-menu li')
+const courses = document.getElementById('courses')
+const dropdown = document.querySelector('.sub-nav')
+const loginBtn = document.querySelector('.header-btn')
+const popup = document.querySelector('.popup')
 
-let menuItems = document.querySelectorAll('.nav-menu li')
-let span = document.createElement('span')
+
+// animation border-bottom (for header items)
+const span = document.createElement('span')
 span.classList.add('highlight')
 document.body.appendChild(span)
 menuItems.forEach(item => {
     item.addEventListener('mouseenter', (ev) => {
         let itemCoordinates = ev.target.getBoundingClientRect()
-        let {left,width,bottom} = itemCoordinates
+        let {left, width, bottom} = itemCoordinates
         span.style.width = `${width}px`
         span.style.transform = `translate(${left}px,${bottom}px)`
     })
@@ -15,12 +20,16 @@ menuItems.forEach(item => {
 
 
 // dropdown (sub-nav)
-const courses = document.getElementById('courses')
-const dropdown = document.querySelector('.sub-nav')
-
-courses.addEventListener('mouseover',()=>{
+courses.addEventListener('mouseover', () => {
     dropdown.style.display = 'flex'
 })
-dropdown.addEventListener('mouseleave',()=>{
+dropdown.addEventListener('mouseleave', () => {
     dropdown.style.display = 'none'
+})
+
+
+// login popup(modal)
+loginBtn.addEventListener('click', () => {
+    popup.style.display = 'flex'
+    popup.style.opacity = '1'
 })
